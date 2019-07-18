dc697553b69d761fe561166a14a0dd8d9f849748
Megat Syafiq <syafiqq95@gmail.com>
2019-07-18 11:29:18 +0800

    first commit

---
+++ TodoList/TodoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TodoListViewController.swift
+//  TodoList
+//
+//  Created by Megat Syafiq on 13/07/2019.
+//  Copyright © 2019 Megat Syafiq. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
